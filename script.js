0400069bae1f3f882255dec20428749600962d31
gfaerny <120312426+Gfaerny@users.noreply.github.com>
2024-07-20 13:24:59 -0700

    Update script.js

--- script.js
+++ script.js
@@ -4,4 +4,13 @@ const showcase = document.querySelector(".showcase");
 menuToggle.addEventListener("click", () => {
   menuToggle.classList.toggle("active");
   showcase.classList.toggle("active");
+
+  // Add additional functionality here
+  if (showcase.classList.contains("active")) {
+    // If showcase is active, do something
+    console.log("Showcase is active");
+  } else {
+    // If showcase is not active, do something else
+    console.log("Showcase is not active");
+  }
 });
